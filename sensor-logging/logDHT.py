d775cfb9ced82fffed745f18060ad2dab085c545
EZ-maas2 <elizaveta.zavyalova0u0@gmail.com>
2022-01-07 01:44:41 +0100

    modified for reading data from arduino moisture sensors and adding it to the sensor log

--- sensor-logging/logDHT.py
+++ sensor-logging/logDHT.py
@@ -1,42 +1,67 @@
 import sqlite3 as lite
-import sys
 import Adafruit_DHT
-import time
+from pyfirmata import Arduino, util
+from time import sleep
 
+
+
+# sleep time
 fs=1
+
+# ----------------------------------------
+# PINS
+DHT_PIN = 4   # GPIO4
+ArduinoPins = 4 # A0, A1, A2, A3
+
+board = Arduino('/dev/ttyUSB0')
+# ----------------------------------------
 
 con = lite.connect('/home/pi/Documents/Sensor_Database/DHT_data.db')
 with con:
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS DHT_data")
-    cur.execute("CREATE TABLE DHT_data(timestamp DATETIME, hum NUMERIC, temp NUMERIC)")
+    # add four additional columns for the moisture sensor data
+    cur.execute("CREATE TABLE DHT_data(timestamp DATETIME, hum NUMERIC, temp NUMERIC, moist1 NUMERIC, moist2 NUMERIC,\
+    moist3 NUMERIC, moist4 NUMERIC,)")
 
 
 def read_DHTdata():
     DHT_SENSOR = Adafruit_DHT.DHT22
-    DHT_PIN = 4
     
     hum, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    moist = readMoist()
     if hum is not None and temp is not None:
         hum = round(hum, 2)
         temp = round(temp, 2)
-        insert_data(hum, temp)
-    return hum, temp
+        insert_data(hum, temp, moist)
+
 
     
-def insert_data(hum, temp):
+def insert_data(hum, temp, moist):
     cur.execute("INSERT INTO DHT_data values(datetime('now'),?,?)", (hum,temp))
+    for i in range(ArduinoPins):
+        cur.execute("INSERT INTO DHT_data values(?)", moist[i])
     con.commit()
-    
+
+def readMoist():
+    it = util.Iterator(board)
+    it.start()
+    moist = []
+    for i in range(ArduinoPins):
+        board.analog[i].enable_reporting()
+        moist[i] = board.analog[i].read()
+
+    return moist
+
+
 
 def main():
     
     while True:
-        hum, temp = read_DHTdata()
-        insert_data(hum, temp)
+        read_DHTdata()
 #         for row in cur.execute("SELECT * FROM DHT_data"):
 #             print (row)
-        time.sleep(fs)
+        sleep(fs)
 
 main()
     
