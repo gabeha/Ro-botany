4561dd4123f0007b6e37ad7695a4bcf88c765ac0
Szabolcs Meszaros <93373750+skandar98@users.noreply.github.com>
2022-01-14 11:59:01 +0100

    Update Stepper motor.py

--- Stepper motor.py
+++ Stepper motor.py
@@ -2,42 +2,41 @@
 import pyfirmata
 import time
 
+class motor(object):
+
+    def __init__(self):
+        
+        self.A1 = board.get_pin('d:3:p')
+        self.A2 = board.get_pin('d:5:p')
+        self.B1 = board.get_pin('d:6:p')
+        self.B2 = board.get_pin('d:9:p')
+
+    def setStepper(self, in1, in2, in3, in4):
+        self.A1.write(in1)
+        self.A2.write(in2)
+        self.B1.write(in3)
+        self.B2.write(in4)
+
+    def forwardStep(self):
+        self.setStepper(1, 0, 1, 0)
+        self.setStepper(0, 1, 1, 0)
+        self.setStepper(0, 1, 0, 1)
+        self.setStepper(1, 0, 0, 1)
+
+    def backwardStep(self):
+        self.setStepper(1, 0, 0, 1)
+        self.setStepper(0, 1, 0, 1)
+        self.setStepper(0, 1, 1, 0)
+        self.setStepper(1, 0, 1, 0)
 
 
-
-def setupX():
-    X_A1 = board.get_pin('d:3:p')
-    X_A2 = board.get_pin('d:5:p')
-    X_B1 = board.get_pin('d:6:p')
-    X_B2 = board.get_pin('d:9:p')
-
-def setStepperX(in1, in2, in3, in4):
-    X_A1.write(in1)
-    X_A2.write(in2)
-    X_B1.write(in3)
-    X_B2.write(in4)
-
-def forwardStepX():
-    setStepperX(1, 0, 1, 0)
-    setStepperX(0, 1, 1, 0)
-    setStepperX(0, 1, 0, 1)
-    setStepperX(1, 0, 0, 1)
-
-def backwardStepX():
-    setStepperX(1, 0, 0, 1)
-    setStepperX(0, 1, 0, 1)
-    setStepperX(0, 1, 1, 0)
-    setStepperX(1, 0, 1, 0)
-
-i=0
 if __name__=='__main__':
   board = pyfirmata.Arduino('/dev/ttyACM0')
   print("it doesnt work")
   it = pyfirmata.util.Iterator(board)
   it.start()
 
-setupX()  # setup motor for x coordinate movement
+motor1 = motor()  # setup motor for x coordinate movement
 
-while i <= 10:
-    forwardStepX()
-    i += 1
+for i in range(10):
+    motor1.forwardStep()
