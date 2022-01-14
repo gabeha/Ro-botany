c88097cb7b926466ed08bb032141c45d9e3783f7
bindia99 <48791915+bindia99@users.noreply.github.com>
2022-01-14 15:02:29 +0100

    Add files via upload
    motor code accel

---
+++ sketch_jan14a.ino
@@ -0,0 +1,21 @@
+// Include the AccelStepper library:
+#include <AccelStepper.h>
+// Motor pin definitions:
+#define motorPin1  22     // IN1 on the ULN2003 driver
+#define motorPin2  24     // IN2 on the ULN2003 driver
+#define motorPin3  26     // IN3 on the ULN2003 driver
+#define motorPin4  28     // IN4 on the ULN2003 driver
+// Define the AccelStepper interface type; 4 wire motor in half step mode:
+#define MotorInterfaceType 8
+// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper library with 28BYJ-48 stepper motor:
+AccelStepper stepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, motorPin2, motorPin4);
+void setup() {
+  // Set the maximum steps per second:
+  stepper.setMaxSpeed(2000);
+}
+void loop() {
+  // Set the speed of the motor in steps per second:
+  stepper.setSpeed(1000);
+  // Step the motor with constant speed as set by setSpeed():
+  stepper.runSpeed();
+}
